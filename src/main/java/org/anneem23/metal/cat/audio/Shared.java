a2319609de757da1adaaac2c525520f3e3e49475
Anne Milbert <anneem23@gmail.com>
2016-06-19 15:14:34 +0200

    applied standard sonar rules

--- src/main/java/org/anneem23/metal/cat/audio/Shared.java
+++ src/main/java/org/anneem23/metal/cat/audio/Shared.java
@@ -26,10 +26,10 @@ package org.anneem23.metal.cat.audio;
 
 import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Mixer;
 import javax.sound.sampled.Mixer.Info;
-import java.io.UnsupportedEncodingException;
-import java.nio.charset.Charset;
-import java.util.Vector;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Shared {
 
@@ -37,48 +37,26 @@ public class Shared {
 	public static final int HOPSIZE = 512;
 	public static final int FRAME_SIZE = 2*HOPSIZE;
 	public static final AudioFormat AUDIO_FORMAT = new AudioFormat(SAMPLE_RATE, 16, 1, true, false);
+
+    private Shared() {}
 	
-	public static Vector<Info> getMixerInfo(
+	public static List<Info> getMixerInfo(
 			final boolean supportsPlayback, final boolean supportsRecording) {
-		final Vector<Info> infos = new Vector<>();
+		final List<Info> infos = new ArrayList<>();
 		final Info[] mixers = AudioSystem.getMixerInfo();
 		for (final Info mixerinfo : mixers) {
-			if (supportsRecording
-					&& AudioSystem.getMixer(mixerinfo).getTargetLineInfo().length != 0) {
-				// Mixer capable of recording audio if target LineWavelet length != 0
-				infos.add(mixerinfo);
-			} else if (supportsPlayback
-					&& AudioSystem.getMixer(mixerinfo).getSourceLineInfo().length != 0) {
-				// Mixer capable of audio play back if source LineWavelet length != 0
-				infos.add(mixerinfo);
+			try (Mixer mixer = AudioSystem.getMixer(mixerinfo)) {
+				if (supportsRecording
+						&& mixer.getTargetLineInfo().length != 0) {
+					// Mixer capable of recording audio if target LineWavelet length != 0
+					infos.add(mixerinfo);
+				} else if (supportsPlayback
+						&& mixer.getSourceLineInfo().length != 0) {
+					// Mixer capable of audio play back if source LineWavelet length != 0
+					infos.add(mixerinfo);
+				}
 			}
 		}
 		return infos;
-	}
-
-	public static String toLocalString(Object info)
-	{
-		if(!isWindows())
-			return info.toString();
-		String defaultEncoding = Charset.defaultCharset().toString();
-		try
-		{
-			return new String(info.toString().getBytes("windows-1252"), defaultEncoding);
-		}
-		catch(UnsupportedEncodingException ex)
-		{
-			return info.toString();
-		}
-	}
-	private static String OS = null;
-	public static String getOsName()
-	{
-		if(OS == null)
-			OS = System.getProperty("os.name");
-	    return OS;
-	}
-	public static boolean isWindows()
-	{
-	   return getOsName().startsWith("Windows");
 	}
 }
